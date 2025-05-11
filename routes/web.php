f14fe9ed3c3db53bf53d63a7ba1fba4b222a33fa
Gurpreet Singh <connect.gurpreet@outlook.com>
2025-05-11 20:50:11 +0530

    comments updated

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProductController;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     $products = Product::with('featured_image')->inRandomOrder()->take(12)->get();
     $featuredProducts = Product::with('featured_image')->inRandomOrder()->take(2)->get();
@@ -15,6 +16,8 @@ Route::get('/', function () {
 })->name('home');
 
 // Auth Routes
+// These routes are only accessible to unauthenticated users.
+// If a user is authenticated, they will be redirected to the home page.
 Route::middleware('guest')->group(function () {
     Route::get('login', [AuthController::class, 'login'])->name('login');
     Route::post('login', [AuthController::class, 'authenticate'])->name('login.post');
@@ -23,12 +26,14 @@ Route::middleware('guest')->group(function () {
     Route::get('forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
 });
 
-
+// These routes are only accessible to authenticated users.
+// If a user is not authenticated, they will be redirected to the login page.
 Route::middleware('auth')->group(function () {
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 });
 
 // Product Routes
+// These routes are accessible to all users, regardless of authentication status.
 Route::prefix('products')->group(function () {
     Route::get('/{slug}', [ProductController::class, 'show'])->name('product.show');
 });
